f843886076ec017ebef3b7a6395f94432672b2e0
João_de_Oliveira <82655975+JoaoVOliveira2003@users.noreply.github.com>
2024-11-21 20:21:29 -0300

    Add files via upload

---
+++ Arduino/piscaPisca.c
@@ -0,0 +1,13 @@
+#define led 2; //defina 2 como le
+
+void setup() {
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(1000);
+}
